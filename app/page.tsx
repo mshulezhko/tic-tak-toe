5002ac1c0d0ed2ef8671b0a4a636b8426b9ef556
mshulezhko <119418285+mshulezhko@users.noreply.github.com>
2024-01-25 22:43:10 +0100

    add components for test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-import Board from "@/components/Board";
+// import Board from "@/components/Board";
+// import SingleGameBoard from "@/components/SingleGame/SingleGameBoard";
+import ChooseIcon from "@/components/SingleGame/ChooseIcon";
+import HomePage from "@/components/HomePage/HomePage";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
-      <Board />
+      {/* <Board /> */}
+      {/* <SingleGameBoard /> */}
+      <ChooseIcon />
+      <HomePage />
     </main>
   );
 }
