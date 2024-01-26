b66b1010a7260831c07b0d392320601f0b78ff84
mshulezhko <119418285+mshulezhko@users.noreply.github.com>
2024-01-26 21:32:32 +0100

    create new functional: fonctional for save data to local storage for multi game mode

---
+++ app/utils/storage.tsx
@@ -0,0 +1,70 @@
+export type GameMode = {
+  isSingleMode: boolean;
+  isMultiMode: boolean;
+};
+
+export type Board = {
+  squares: (string | null)[];
+  currentPlayer: string;
+  emoji: null | string;
+};
+
+// export type userDataType = {
+//   mode: GameMode;
+//   board: (string | null)[];
+//   emoji: null | string;
+// };
+
+const saveToLocalStorageMode = (mode: GameMode) => {
+  localStorage.setItem("userDataMode", JSON.stringify(mode));
+};
+
+const getLocalStorageMode = () => {
+  const userDataMode = localStorage.getItem("userDataMode");
+
+  if (userDataMode) return JSON.parse(userDataMode);
+};
+
+const saveLocalStorageBoard = ({
+  squares,
+  currentPlayer,
+  emoji = null,
+}: Board) => {
+  localStorage.setItem(
+    "localStorageBoard",
+    JSON.stringify({ squares, currentPlayer, emoji })
+  );
+};
+
+const getLocalStorageBoard = () => {
+  const localStorageBoard = localStorage.getItem("localStorageBoard");
+
+  if (localStorageBoard) return JSON.parse(localStorageBoard);
+};
+
+const checkLocalStorageBoard = () => {
+  const localStorageBoard: Board = getLocalStorageBoard();
+  const values = localStorageBoard?.squares.filter(
+    (element) => element !== null
+  );
+
+  if (values?.length) {
+    return localStorageBoard;
+  }
+
+  return null;
+};
+
+const cleanLocalStorage = () => {
+  localStorage.removeItem("userDataMode");
+  localStorage.removeItem("localStorageBoard");
+};
+
+export {
+  saveToLocalStorageMode,
+  cleanLocalStorage,
+  getLocalStorageMode,
+  saveLocalStorageBoard,
+  getLocalStorageBoard,
+  checkLocalStorageBoard,
+};
