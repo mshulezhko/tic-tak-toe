477b0ea5a15e020eaedfc51583c38d8c94cbc8e8
mshulezhko <119418285+mshulezhko@users.noreply.github.com>
2024-01-27 17:45:54 +0100

    create footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      This project was coded by Mariia Shulezhko and is{" "}
+      <a href="https://github.com/mshulezhko/tic-tak-toe" target="_blank">
+        {" "}
+        open-sourced in GitHub
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
